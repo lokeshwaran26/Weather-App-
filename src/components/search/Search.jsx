d7d8063adbcc7f4080f79cf6ef66d1529ef14c52
lokesh <lokesh95664@gmail.com>
2023-03-02 21:25:10 +0530

    ..

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { AsyncPaginate } from 'react-select-async-paginate';
-import { GEO_API_URL, geoApiOptions } from '../../api';
+import { GEO_API_URL, geoApiOptions } from '../../api'; // API
 
 const Search = ({onSearchChange}) => {
 
