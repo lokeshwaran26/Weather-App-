8e8ee75bf928e3f680adbc1e206ca5ef97069470
lokesh <lokesh95664@gmail.com>
2023-03-03 22:19:50 +0530

    ...

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -10,7 +10,16 @@ const Search = ({onSearchChange}) => {
       `${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`, geoApiOptions
        )
 	      .then(response => response.json())
-	      .then(response => console.log(response))
+	      .then(response => {
+          return {
+            options : response.data.map((city)=>{
+              return {
+                value: `${city.latitude} ${city.longitude}`,
+                label: `${city.name}, ${city.countryCode}`,
+              }
+            })
+          }
+        })
 	      .catch(err => console.error(err));
   }
   const handleOnChange = (searchData) =>{
