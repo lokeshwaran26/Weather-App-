66a0783addfe4e544fd7c8c85d8d477e831a1639
lokesh <lokesh95664@gmail.com>
2023-03-12 19:58:27 +0530

    ..

--- src/api.jsx
+++ src/api.jsx
@@ -1,10 +1,9 @@
 export const geoApiOptions = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'dc93f2875fmsh2ea8a53074cde47p1d7403jsnb920415435ed',
-		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
-	}
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "dc93f2875fmsh2ea8a53074cde47p1d7403jsnb920415435ed", //rapid API key
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com", //Host
+  },
 };
 
-export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
-
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
